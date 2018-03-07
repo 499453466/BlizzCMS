d3c7967064082b2916665a22ddddd63667bee0ce
Vipo <develop.km14@gmail.com>
2018-03-08 00:24:35 +0200

    UTF8

--- application/modules/armory/config/armory.php
+++ application/modules/armory/config/armory.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
--- application/modules/armory/models/Armory_model.php
+++ application/modules/armory/models/Armory_model.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Armory_model extends CI_Model {
