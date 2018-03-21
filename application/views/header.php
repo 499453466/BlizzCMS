aa89c75fc440f9cde05187d5fe95d7ab208af8fa
Dzywolf <luis.wolf.core@gmail.com>
2018-03-20 23:59:05 -0300

    Fixed order and icons

--- application/views/header.php
+++ application/views/header.php
@@ -6,10 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/images/favicon.ico">
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="<?= base_url(); ?>core/css/blizzcms-general.css">
-    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url('theme/'); ?><?= $this->config->item('theme_name'); ?>/css/<?= $this->config->item('theme_name'); ?>.css"/>
-
     <!-- UIkit -->
     <link rel="stylesheet" href="<?= base_url(); ?>core/uikit/css/uikit.min.css"/>
     <script src="<?= base_url(); ?>core/uikit/js/uikit.min.js"></script>
@@ -18,6 +14,10 @@
     <!-- Font Awesome and Rpg Awesome -->
     <link rel="stylesheet" href="<?= base_url(); ?>core/fontawesome/css/fontawesome-all.css">
     <link rel="stylesheet" href="<?=base_url('core/rpg_awesome/css/rpg-awesome.css')?>">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>core/css/blizzcms-general.css">
+    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url('theme/'); ?><?= $this->config->item('theme_name'); ?>/css/<?= $this->config->item('theme_name'); ?>.css"/>
 
     <!-- Wowhead -->
     <script>var whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
