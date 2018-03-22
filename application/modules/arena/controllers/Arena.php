fb8d11fd8d3c2a6dd8ba5e7257eac1a5824858c0
Vipo <develop.km14@gmail.com>
2018-03-23 00:54:20 +0200

    arena new method

--- application/modules/arena/controllers/Arena.php
+++ application/modules/arena/controllers/Arena.php
@@ -19,16 +19,37 @@ class Arena extends MX_Controller {
         if (!$this->m_permissions->getMyPermissions('Permission_ArenaStats'))
             redirect(base_url(),'refresh');
 
+        $this->config->load('arena');
         $this->load->model('arena_model');
     }
 
     public function index()
     {
-        $data['fxtitle'] = $this->lang->line('nav_arena_statistics');
-        $data['fx_adds'] = '<div class="uk-container">';
+        $data = array(
+            'fxtitle' => $this->lang->line('nav_arena_statistics'),
+            'fx_adds' => '',
+            //lang
+            'nav_arena_statistics' => $this->lang->line('nav_arena_statistics'),
+            'lang_2v2' => $this->lang->line('arena_top_2v2'),
+            'lang_3v3' => $this->lang->line('arena_top_3v3'),
+            'lang_5v5' => $this->lang->line('arena_top_5v5'),
+            'column_team_name' => $this->lang->line('column_team_name'),
+            'column_members' => $this->lang->line('column_members'),
+            'column_rating' => $this->lang->line('column_rating'),
+            //configs
+            'textcolor' => $this->config->item('textcolor'),
+            'uk-tab' => $this->config->item('uk-tab'),
+            //general
+            'realms' => $this->m_data->getRealms()->result(),
+        );
         
         $this->load->view('header', $data);
-        $this->load->view('index');
+        $this->parser->parse('index', $data);
         $this->load->view('footer');
+    }
+
+    public function test()
+    {
+        echo $this->arena_model->getRealmID();
     }
 }
